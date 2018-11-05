b010031411000c07d5f470bf2282313ae0568666
vinaykashyap1996 <vinay.kashyap@accionlabs.com>
2018-11-05 13:50:11 +0530

    Forgot_password

---
+++ model/model.js
@@ -0,0 +1,24 @@
+const mongoose=require('mongoose');
+const bcryt=require('bcrypt');
+const saltrounds=10;
+
+const UserSchema = mongoose.Schema({
+    firstname:String,
+    lastname:String,
+    email:String,
+    password:String,
+    resetPasswordToken: String,
+    resetPasswordExpires: Date
+
+});
+UserSchema.pre('save', function(next){
+    this.password = bcrypt.hashSync(this.password, saltRounds);
+    next();
+    });
+    module.exports = mongoose.model('User', UserSchema);
+const Users = mongoose.model('user', UserSchema);
+UserSchema.plugin(AutoIncrement, {inc_field: 'id'});
+module.exports = {
+    userModel: Users,
+    UserSchema: UserSchema
+}
